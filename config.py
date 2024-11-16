5eacaa148d95f2ce8d1c613cc50a762be0ccd680
Himanshu88990 <haxonnnop@gmail.com>
2024-11-16 14:42:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "26853476"))
 API_HASH = os.environ.get("API_HASH", "6876d59072d00da396a9c0c345de8819")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://haxonnnop:xJfW0QSNweUyKWOt@cluster0.bgi1r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
